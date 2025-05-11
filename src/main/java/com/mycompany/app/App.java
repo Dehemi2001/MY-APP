3811acaaba277371d8a4225b57e49405f21e08d0
Dehemi Patabendige <dp.dehemisuvipul@gmail.com>
2025-05-11 15:43:57 +0530

    Add database connectivity in App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,10 +1,23 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Database URL, username, and password
+        String url = "jdbc:mysql://localhost:3306/practice";
+        String username = "root";
+        String password = "";
+
+        // Establishing a connection
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Connected to the database successfully!");
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to the database.");
+            e.printStackTrace();
+        }
     }
 }
